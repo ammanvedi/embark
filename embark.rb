c77f3f98f21c57042d98a467f39b93dee5366b98
ammanvedi <amman.vedi@gmail.com>
2020-03-17 23:04:19 +0000

    fix echo synopsis

--- embark.rb
+++ embark.rb
@@ -4,8 +4,6 @@ class Embark < Formula
     url "https://github.com/ammanvedi/embark/archive/v1.1.5.tar.gz"
     version "1.1.5"
   
-    depends_on "curl"
-  
     bottle :unneeded
   
     def install
