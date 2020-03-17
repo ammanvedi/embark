64b5b3c4cc6f318601f467ae54b34b3286bce632
ammanvedi <amman.vedi@gmail.com>
2020-03-17 22:31:25 +0000

    [release] 1.1.4 version bump

--- embark.rb
+++ embark.rb
@@ -1,8 +1,8 @@
 class Weather < Formula
     desc "A command line tool to help with git releases"
     homepage "https://github.com/ammanvedi/embark"
-    url "https://github.com/ammanvedi/embark/archive/v1.1.3.tar.gz"
-    version "1.1.3"
+    url "https://github.com/ammanvedi/embark/archive/v1.1.4.tar.gz"
+    version "1.1.4"
   
     depends_on "curl"
   
