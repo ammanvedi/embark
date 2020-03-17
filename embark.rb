e7e57326b06d17d8ae7a7e33efc095842c5c0778
ammanvedi <amman.vedi@gmail.com>
2020-03-17 21:12:00 +0000

    add homebrew and command to update homebrew version

--- embark.rb
+++ embark.rb
@@ -1,9 +1,8 @@
 class Weather < Formula
     desc "A command line tool to help with git releases"
     homepage "https://github.com/ammanvedi/embark"
-    url "https://github.com/ammanvedi/embark/archive/v1.0.1.tar.gz"
-    sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
-    version "1.0.1"
+    url "https://github.com/ammanvedi/embark/archive/v{{version}}.tar.gz"
+    version "{{version}}"
   
     depends_on "curl"
   
