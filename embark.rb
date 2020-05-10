cb9d2c7a41ce41077edd78c94c537b97fd87e23c
amman-vedi.tsx <amman.vedi@gmail.com>
2020-05-10 17:59:56 +0100

    [release] 2.0.1 version bump (#19)

--- embark.rb
+++ embark.rb
@@ -1,8 +1,8 @@
 class Embark < Formula
     desc "A command line tool to help with git releases"
     homepage "https://github.com/ammanvedi/embark"
-    url "https://github.com/ammanvedi/embark/archive/v2.0.00.tar.gz"
-    version "2.0.00"
+    url "https://github.com/ammanvedi/embark/archive/v2.0.10.tar.gz"
+    version "2.0.10"
   
     bottle :unneeded
   
