3d2d4fb192a1b9833541996ae7020fe431485a54
ammanvedi <amman.vedi@gmail.com>
2020-05-10 18:07:09 +0100

    fix rb again

--- embark.rb
+++ embark.rb
@@ -1,7 +1,7 @@
 class Embark < Formula
     desc "A command line tool to help with git releases"
     homepage "https://github.com/ammanvedi/embark"
-    url "https://github.com/ammanvedi/embark/archive/v2.1.00.tar.gz"
+    url "https://github.com/ammanvedi/embark/archive/v2.1.0.tar.gz"
     version "2.1.0"
   
     bottle :unneeded
